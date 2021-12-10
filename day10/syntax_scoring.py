525f38f02da83a437a6b57c2024d47293c130194
Fernando Berra Kossik <fernando@berraming.com>
2021-12-10 11:37:49 -0500

    Improve by going to autocomplete calculation the same time as checking for corruption, this improve the performance

--- day10/syntax_scoring.py
+++ day10/syntax_scoring.py
@@ -18,9 +18,13 @@ CLOSING_CHUNK_MAPPING = {
 }
 AUTOCOMPLETE_SCORE_MAPPING = {
     '(': 1,
+    ')': 1,
     '[': 2,
+    ']': 2,
     '{': 3,
-    '<': 4
+    '}': 3,
+    '<': 4,
+    '>': 4
 }
 OPENING_CHUNKS = '([{<'
 syntax_score = 0
@@ -37,10 +41,14 @@ with open('input_data.txt', 'r') as f:
         line = line.strip()
 
         syntax_queue = []
+        autocomplete_score = 0
 
         for char in line:
             if char in OPENING_CHUNKS:
                 # This is an opening chunk
+                # Add the autocomplete score value based on how many chunks opening we have
+                # in the queue. While more we have we have to increase by an exponential factor of 5
+                autocomplete_score += ((5**len(syntax_queue))*AUTOCOMPLETE_SCORE_MAPPING[char])
                 syntax_queue.append(char)
             else:
                 # This is a closing chunk, it should match the last
@@ -53,20 +61,16 @@ with open('input_data.txt', 'r') as f:
                     # corruption, the last element does not match the closing chunk
                     syntax_score += SYNTAX_SCORE_MAPPING[char]
 
-                    # empty the queue, no need for this other than get incomplete lines later
+                    # empty the queue
                     syntax_queue = []
                     break
                 else:
                     # valid closing chunk, remove element from the queue
                     syntax_queue.pop()
+                    # Given this is a closing chunk, decrease the autocomplete score
+                    autocomplete_score -= ((5**len(syntax_queue))*AUTOCOMPLETE_SCORE_MAPPING[char])
 
         if len(syntax_queue) > 0:
-            autocomplete_score = 0
-            while syntax_queue:
-                char = syntax_queue.pop()
-                autocomplete_score *= 5
-                autocomplete_score += AUTOCOMPLETE_SCORE_MAPPING[char]
-
             autocomplete_scores.append(autocomplete_score)
 
         line = f.readline()
